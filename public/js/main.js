22bf20091e1bbcd58a03e10868cf7971693c967e
vincent scotto <vincent@scotto.ws>
2017-11-10 10:23:53 -0800

    Added a few styles

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,16 @@
-// const deleteCategoryButton = document.querySelector('.delete-category');
-// const deleteArticleButton = document.querySelector('.delete-article');
-
-// console.log(deleteArticleButton)
-
-//   deleteCategoryButton.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     $target = $(e.target);
-
-//     $.ajax({
-//       type: 'DELETE',
-//       url: '/delete/category/' + $target.data('cat-id'),
-//       success: (response) => {
-//         alert('Category Deleted');
-//         window.location.href = '/'
-//       },
-//       error: (error) => {
-//         console.log(error);
-//       }
-//     });
-//   });
-
-
-
-//   deleteArticleButton.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     $target = $(e.target);
-
-//     $.ajax({
-//       type: 'DELETE',
-//       url: '/delete/article/' + $target.data('article-id'),
-//       success: (response) => {
-//         alert('Article Deleted');
-//         window.location.href = '/'
-//       },
-//       error: (error) => {
-//         console.log(error);
-//       }
-//     });
-//   }); 
-
-
-
 $(document).ready(() => {
+
+// add class for nav
+  $(window).scroll(function() {
+     if($(window).scrollTop() >= 100) {
+       $('nav').addClass('scrolled');
+     }
+    else {
+      $('nav').removeClass('scrolled');
+    }
+  });
+
+// delete category
   $('.delete-category').on('click', (e) => {
     $target = $(e.target);
     $.ajax({
@@ -56,6 +25,8 @@ $(document).ready(() => {
       }
     })
   });
+
+// delete article
   $('.delete-article').on('click', (e) => {
     $target = $(e.target);
     $.ajax({
@@ -72,14 +43,3 @@ $(document).ready(() => {
   });
 
 });
-
-
-
-
-
-
-
-
-
-
-
